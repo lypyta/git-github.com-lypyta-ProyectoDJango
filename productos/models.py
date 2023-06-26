9f5629bc7fca6569a2f05b958176ce05adb6e528
Lye <128102610+lypyta@users.noreply.github.com>
2023-06-26 19:21:16 -0400

    migracion de tablas y creacion archivo 0001_init

--- productos/models.py
+++ productos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Servicio(models.model):
-    nombre_s = models.CharField(max_length=80)
-
-class Cliente(models.model):
+class Servicio(models.Model):
+    nombre_s = models.CharField(max_length=80) 
+       
+class Cliente(models.Model):
     nombre_c = models.CharField(max_length=80)
     tipo_cliente = models.CharField(max_length=80)
     rut = models.CharField(max_length=16)
